63e51b35d5fe7e65d667493b83a611ae51d85923
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-06 00:32:45 +0300

    my first fetch request

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,6 +7,7 @@ import RingCategory from "./RingCategory";
 import WatchCategory from "./WatchCategory";
 import ShoppingCart from "./ShoppingCart";
 import "./App.css";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/shoppingcart" element={<ShoppingCart />} />
         </Routes>
       </div>
+      <Footer />
     </>
   );
 }
--- src/Components/RingCategory.js
+++ src/Components/RingCategory.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./Footer";
 
 function RingCategory() {
+  const [rings, setRings] = useState([]);
+  // Add useEffect hook
+  useEffect(() => {
+    fetch("http://localhost:3000/rings")
+      .then((r) => r.json())
+      .then((items) => console.log(items));
+  }, []);
   return (
     <div className="ring">
       <br />
