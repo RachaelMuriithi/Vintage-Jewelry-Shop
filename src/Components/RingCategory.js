52b2d1626c2aa72ca7932de6c21fb5047b0bfcb4
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-06 03:36:56 +0300

    done with my ring category

--- src/Components/RingCategory.js
+++ src/Components/RingCategory.js
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import Footer from "./Footer";
-
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardGroup from 'react-bootstrap/CardGroup';
 function RingCategory() {
   const [rings, setRings] = useState([]);
   // Add useEffect hook
   useEffect(() => {
     fetch("http://localhost:3000/rings")
       .then((r) => r.json())
-      .then((items) => console.log(items));
+      .then((rings) => setRings(rings));
   }, []);
+
   return (
     <div className="ring">
       <br />
-      <h1></h1>
-      <div>
-        <Footer />
-      </div>
+      {rings.map((ring, id) => {
+        return (
+          <div className="rings">
+            <CardGroup style={{ width: "48rem" }}>
+              <Card.Img variant="top" src={ring.url} alt={ring.title} /> <br/> <br/>
+              <Card.Body>
+                <Card.Title style={{fontWeight: "600",color: "whitesmoke",fontSize: "20px"}} key="ring.id">{ring.title}</Card.Title>
+                <Card.Text style={{fontWeight: "400",color: "whitesmoke",fontSize: "19px"}}>{ring.description}</Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </CardGroup>
+          </div>
+        );
+      })}
     </div>
   );
 }
