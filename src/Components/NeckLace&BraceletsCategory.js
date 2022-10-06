ac140249df7b584755e203e72ddbcf38e4024b3d
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-06 12:08:10 +0300

    fetch request for all my categories

--- src/Components/NeckLace&BraceletsCategory.js
+++ src/Components/NeckLace&BraceletsCategory.js
@@ -1,15 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Footer from "./Footer";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
 function NecklaceCategory() {
+  const [necklaces, setNecklaces] = useState([]);
+  // Add useEffect hook
+  useEffect(() => {
+    //fetch the ring items
+    fetch("http://localhost:3000/necklaces")
+      .then((r) => r.json())
+      .then((necklaces) => setNecklaces(necklaces));
+  }, []);
+
   return (
-    <div className="neckLace">
+    <div className="necklace">
       <br />
-      <h1></h1>
-      <div>
-        <Footer />
-      </div>
+      {/* //display the ring items */}
+      {necklaces.map((necklace, id) => {
+        return (
+          <div className="necklaces">
+            <CardGroup style={{ width: "48rem" }}>
+              <Card.Img variant="top" src={necklace.url} alt={necklace.title} /> <br />
+              <br />
+              <Card.Body>
+                <Card.Title
+                  style={{
+                    fontWeight: "600",
+                    color: "whitesmoke",
+                    fontSize: "20px",
+                  }}
+                  key="necklace.id"
+                >
+                  {necklace.title}
+                </Card.Title>
+                <Card.Text
+                  style={{
+                    fontWeight: "400",
+                    color: "whitesmoke",
+                    fontSize: "19px",
+                  }}
+                >
+                  {necklace.description}
+                </Card.Text>
+                <Button
+                  style={{
+                    width: "13rem",
+                    height: "2rem",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    backgroundColor: "rgb(184,162,125)",
+                  }}
+                  variant="primary"
+                >
+                  Add to Cart
+                </Button>
+              </Card.Body>
+            </CardGroup>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/Components/WatchCategory.js
+++ src/Components/WatchCategory.js
@@ -1,16 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Footer from "./Footer";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+
 
 function WatchCategory() {
+
+       const [watches, setWatches] = useState([]);
+  // Add useEffect hook
+  useEffect(() => {
+    //fetch the watch items
+    fetch("http://localhost:3000/watches")
+      .then((r) => r.json())
+      .then((watches) => setWatches(watches));
+  }, []);
+
   return (
     <div className="watch">
-      {" "}
       <br />
-      <h1></h1>
-      <div>
-        <Footer />
-      </div>
+      {/* //display the ring items */}
+      {watches.map((watch, id) => {
+        return (
+          <div className="watches">
+            <CardGroup style={{ width: "48rem" }}>
+              <Card.Img variant="top" src={watch.url} alt={watch.title} /> <br />
+              <br />
+              <Card.Body>
+                <Card.Title
+                  style={{
+                    fontWeight: "600",
+                    color: "whitesmoke",
+                    fontSize: "20px",
+                  }}
+                  key="ring.id"
+                >
+                  {watch.title}
+                </Card.Title>
+                <Card.Text
+                  style={{
+                    fontWeight: "400",
+                    color: "whitesmoke",
+                    fontSize: "19px",
+                  }}
+                >
+                  {watch.description}
+                </Card.Text>
+                <Button
+                  style={{
+                    width: "13rem",
+                    height: "2rem",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    backgroundColor: "rgb(184,162,125)",
+                  }}
+                  variant="primary"
+                >
+                  Add to Cart
+                </Button>
+              </Card.Body>
+            </CardGroup>
+          </div>
+        );
+      })}
     </div>
   );
 }
