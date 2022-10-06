d5fb0a0ce673cfa7ce09cd66c319b837700fc8be
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-06 04:09:13 +0300

    comment my code

--- src/Components/RingCategory.js
+++ src/Components/RingCategory.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import CardGroup from 'react-bootstrap/CardGroup';
+import CardGroup from "react-bootstrap/CardGroup";
+
 function RingCategory() {
   const [rings, setRings] = useState([]);
   // Add useEffect hook
   useEffect(() => {
+    //fetch the ring items
     fetch("http://localhost:3000/rings")
       .then((r) => r.json())
       .then((rings) => setRings(rings));
@@ -15,15 +17,45 @@ function RingCategory() {
   return (
     <div className="ring">
       <br />
+      {/* //display the ring items */}
       {rings.map((ring, id) => {
         return (
           <div className="rings">
             <CardGroup style={{ width: "48rem" }}>
-              <Card.Img variant="top" src={ring.url} alt={ring.title} /> <br/> <br/>
+              <Card.Img variant="top" src={ring.url} alt={ring.title} /> <br />{" "}
+              <br />
               <Card.Body>
-                <Card.Title style={{fontWeight: "600",color: "whitesmoke",fontSize: "20px"}} key="ring.id">{ring.title}</Card.Title>
-                <Card.Text style={{fontWeight: "400",color: "whitesmoke",fontSize: "19px"}}>{ring.description}</Card.Text>
-                <Button style={{width: "13rem", height: "2rem",fontSize: "16px",fontWeight: "600",backgroundColor: "rgb(184,162,125)"}} variant="primary">Add to Cart</Button>
+                <Card.Title
+                  style={{
+                    fontWeight: "600",
+                    color: "whitesmoke",
+                    fontSize: "20px",
+                  }}
+                  key="ring.id"
+                >
+                  {ring.title}
+                </Card.Title>
+                <Card.Text
+                  style={{
+                    fontWeight: "400",
+                    color: "whitesmoke",
+                    fontSize: "19px",
+                  }}
+                >
+                  {ring.description}
+                </Card.Text>
+                <Button
+                  style={{
+                    width: "13rem",
+                    height: "2rem",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    backgroundColor: "rgb(184,162,125)",
+                  }}
+                  variant="primary"
+                >
+                  Add to Cart
+                </Button>
               </Card.Body>
             </CardGroup>
           </div>
