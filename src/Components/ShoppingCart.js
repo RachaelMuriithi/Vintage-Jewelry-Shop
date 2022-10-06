85d05765f3aec4298b4f7d33bf6ee8fd1e6b4d50
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-10-07 02:26:40 +0300

    comment out cart code

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -13,13 +13,16 @@ function ShoppingCart() {
     removeItem,
     emptyCart,
   } = useCart();
+  //display when cart is empty
   if (isEmpty) return <h1 className="text-center">Your Cart is Empty</h1>;
+  //on add to cart
   return (
-    <section className="py-4 container"> <br/>
+    <section className="py-4 container">
+      <br />
       <div className="row justify-content-center">
         <div className="col-12">
           <h5>
-           Cart ({totalUniqueItems}) total Items: ({totalItems}){" "}
+            Cart ({totalUniqueItems}) total Items: ({totalItems})
           </h5>
           <table className="table table-light table-hover m-0">
             <tbody>
@@ -27,36 +30,50 @@ function ShoppingCart() {
                 return (
                   <tr key={id}>
                     <td>
-                      <img src={item.url} style={{ height: "10rem", marginBottom: "4%", marginRight: "5%" }} />
+                      <img
+                        src={item.url}
+                        style={{
+                          height: "10rem",
+                          marginBottom: "4%",
+                          marginRight: "5%",
+                        }}
+                      />
                     </td>
                     <td>{item.title}</td>
-                    <td><strong>{item.price}$</strong></td>
-                    <td><strong>Qty {item.quantity}</strong></td>
+                    <td>
+                      <strong>{item.price}$</strong>
+                    </td>
+                    <td>
+                      <strong>Qty {item.quantity}</strong>
+                    </td>
+                    {/*Decrease item quantity */}
                     <td>
                       <button
                         className="btn btn-info ms-2"
-                        style={{ height: "2rem"}}
+                        style={{ height: "2rem" }}
                         onClick={() =>
                           updateItemQuantity(item.id, item.quantity - 1)
                         }
                       >
-                        <strong>-</strong> 
+                        <strong>-</strong>
                       </button>
+                      {/* increase item quantity */}
                       <button
                         className="btn btn-info ms-2"
-                        style={{ height: "2rem"}}
+                        style={{ height: "2rem" }}
                         onClick={() =>
                           updateItemQuantity(item.id, item.quantity + 1)
                         }
                       >
-                       <strong>+</strong> 
+                        <strong>+</strong>
                       </button>
+                      {/* Remove item from cart */}
                       <button
                         className="btn btn-danger ms-2"
-                        style={{ height: "2rem"}}
+                        style={{ height: "2rem" }}
                         onClick={() => removeItem(item.id)}
                       >
-                        <strong>Remove Item</strong> 
+                        <strong>Remove Item</strong>
                       </button>
                     </td>
                   </tr>
@@ -69,11 +86,12 @@ function ShoppingCart() {
           <h2>Total Price: {cartTotal} $</h2>
         </div>
         <div className="col-auto">
-              <button 
-              className="btn btn-danger m-2"
-              onClick={() => emptyCart()}
-              ><strong>Clear Cart</strong></button>
-              <button className="btn btn-primary m-2"><strong>Buy Now</strong></button>
+          <button className="btn btn-danger m-2" onClick={() => emptyCart()}>
+            <strong>Clear Cart</strong>
+          </button>
+          <button className="btn btn-primary m-2">
+            <strong>Buy Now</strong>
+          </button>
         </div>
       </div>
     </section>
